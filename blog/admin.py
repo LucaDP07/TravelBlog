eb4dded5f26ccaf05b8b3018768b74344583f1c4
LucaDP07 <luke07@hotmail.it>
2022-10-25 22:30:29 +0000

    create forms file

--- blog/admin.py
+++ blog/admin.py
@@ -17,7 +17,7 @@ class PostAdmin(SummernoteModelAdmin):
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'body', 'post','created_on', 'approved')
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
     search_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
--- blog/models.py
+++ blog/models.py
@@ -29,15 +29,16 @@ class Post(models.Model):
 
 class Comment(models.Model):
 
-        post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comments')
-        name = models.CharField(max_length=80)
-        email = models.EmailField()
-        body = models.TextField()
-        created_on = models.DateTimeField(auto_now_add=True)
-        approved = models.BooleanField(default=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
-        class Meta:
-            ordering = ["created_on"]
 
-        def __str__(self):
-            return f"Comment {self.body} by {self.name}"
+class Meta:
+    ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
