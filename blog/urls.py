391b32a7411569ff6a8fb35b81182d2d44ece3e0
LucaDP07 <luke07@hotmail.it>
2022-11-17 22:36:57 +0000

    create mytrip page where logged in users can add new articles

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('mytrips/', views.MyTripList.as_view(), name='mytrips'),
+    path('tripcreate/', views.TripCreate.as_view(), name='tripcreate'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from .models import Post
 from .forms import CommentForm
 
@@ -68,6 +71,7 @@ class PostDetail(View):
             },
         )
 
+
 class PostLike(View):
     
     def post(self, request, slug, *args, **kwargs):
@@ -81,3 +85,34 @@ class PostLike(View):
             messages.success(request, 'You gave this post a favourite star!')
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+class MyTripList(LoginRequiredMixin, generic.ListView):
+    """
+    Displays logged in user's own recipes
+    """
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'mytrips.html'
+    context_object_name = 'blog'
+
+
+class TripCreate(
+                  SuccessMessageMixin, LoginRequiredMixin, generic.CreateView):
+    """
+    Logged in user can create a recipe and add to my recipes list
+    """
+    model = Post
+    fields = ['trip_image', 'title', 'text', ]
+    template_name = 'mytrips.html'
+    success_url = reverse_lazy('mytrips')
+    success_message = "You have successfully created a post!"
+
+    def form_valid(self, form):
+        """
+        Sets logged in user as author field in form
+        Sets form default status to published
+        """
+        form.instance.author = self.request.user
+        form.instance.status = 1
+        return super(TripCreate, self).form_valid(form)
