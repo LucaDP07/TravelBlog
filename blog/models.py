8ca4753a28751fb50c8a9bb939a4a49ff8033a0b
LucaDP07 <luke07@hotmail.it>
2022-11-24 20:12:59 +0000

    add if statement to show background image with overlay text and introduction only for first page

--- blog/models.py
+++ blog/models.py
@@ -9,14 +9,14 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200,unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('image', blank=False)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
-    content = models.TextField(blank=True,)
+    content = models.TextField(blank=False,)
 
     class Meta:
         ordering = ['-created_on']
