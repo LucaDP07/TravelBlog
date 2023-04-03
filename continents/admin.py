bd252dad3a511f878c4731f255ada890393a4f42
LucaDP07 <luke07@hotmail.it>
2023-04-03 21:33:04 +0000

    styled profile page and navbar

--- continents/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Continent
-
-
-admin.site.register(Continent)
--- continents/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.continents_all, name='continents'),
-]
